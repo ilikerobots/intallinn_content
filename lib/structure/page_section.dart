daeae2fe3a311fc7eeb2abd2c19e4883f55f095c
ilikerobots <mike@hoolehan.com>
2017-03-05 10:56:20 +0200

    Include icon_data info for sections; get section from site structure

--- lib/structure/page_section.dart
+++ lib/structure/page_section.dart
@@ -1,13 +1,14 @@
 import 'package:in_tallinn_content/photo/photo_id.dart';
 
-class PageSection {
+class PageSection implements Comparable<PageSection>{
   String id;
   String title;
   String color;
   String icon;
+  int icon_data;
   PhotoId photoId;
 
-  PageSection(this.id, this.title, {this.icon: "link"}) {
+  PageSection(this.id, this.title, {this.icon: "link", this.icon_data: 0xe157}) {
     photoIdString = id;
   }
 
@@ -15,4 +16,6 @@ class PageSection {
     photoId = new PhotoId(s, type: PhotoCategory.section);
   }
 
+  @override
+  int compareTo(PageSection other) => id.toLowerCase().compareTo(other.id.toLowerCase());
 }
--- lib/structure/site_structure.dart
+++ lib/structure/site_structure.dart
@@ -5,38 +5,38 @@ import 'package:in_tallinn_content/structure/content_page.dart';
 class SiteStructure {
   static final List<Page> pages = <Page>[
     new ContentPage("Basics")
-      ..sections.add(new PageSection("history", "History", icon: "restore"))
-      ..sections.add(new PageSection("people", "People", icon: "people"))
-      ..sections.add(new PageSection("climate", "Climate", icon: "wb_sunny"))
-      ..sections.add(new PageSection("language", "Language", icon: "mode_comment"))
-      ..sections.add(new PageSection("currency", "Currency", icon: "euro_symbol"))
-      ..sections.add(new PageSection("safety", "Safety", icon: "traffic"))
-      ..sections.add(new PageSection("power", "Power", icon: "power"))
-      ..sections.add(new PageSection("internet", "Internet/WiFi", icon: "wifi")),
+      ..sections.add(new PageSection("history", "History", icon: "restore", icon_data: 0xe8b3 ))
+      ..sections.add(new PageSection("people", "People", icon: "people", icon_data: 0xe7fb))
+      ..sections.add(new PageSection("climate", "Climate", icon: "wb_sunny", icon_data: 0xe430))
+      ..sections.add(new PageSection("language", "Language", icon: "mode_comment", icon_data: 0xe253))
+      ..sections.add(new PageSection("currency", "Currency", icon: "euro_symbol", icon_data: 0xe926))
+      ..sections.add(new PageSection("safety", "Safety", icon: "traffic", icon_data: 0xe565))
+      ..sections.add(new PageSection("power", "Power", icon: "power", icon_data: 0xe63c))
+      ..sections.add(new PageSection("internet", "Internet/WiFi", icon: "wifi", icon_data: 0xe63e)),
     new ContentPage("Transport")
-      ..sections.add(new PageSection("airport", "Airport", icon: "airplanemode_active"))
-      ..sections.add(new PageSection("seaport", "Sea Port", icon: "directions_boat"))
-      ..sections.add(new PageSection("auto-rental", "Auto Rental", icon: "directions_car"))
-      ..sections.add(new PageSection("public-transport", "Public Transport", icon: "directions_transit"))
-      ..sections.add(new PageSection("taxis", "Taxis", icon: "local_taxi"))
-      ..sections.add(new PageSection("bikes", "Bikes", icon: "directions_bike")),
+      ..sections.add(new PageSection("airport", "Airport", icon: "airplanemode_active", icon_data: 0xe195))
+      ..sections.add(new PageSection("seaport", "Sea Port", icon: "directions_boat", icon_data: 0xe532))
+      ..sections.add(new PageSection("auto-rental", "Auto Rental", icon: "directions_car", icon_data: 0xe531))
+      ..sections.add(new PageSection("public-transport", "Public Transport", icon: "directions_transit", icon_data: 0xe535))
+      ..sections.add(new PageSection("taxis", "Taxis", icon: "local_taxi", icon_data: 0xe559))
+      ..sections.add(new PageSection("bikes", "Bikes", icon: "directions_bike", icon_data: 0xe52f)),
     new ContentPage("Sightseeing")
-      ..sections.add(new PageSection("tourist-office", "Tourist Office", icon: "info_outline"))
-      ..sections.add(new PageSection("parks", "Parks", icon: "nature_people"))
-      ..sections.add(new PageSection("neighborhoods", "Neighborhoods", icon: "zoom_out_map"))
-      ..sections.add(new PageSection("museums", "Museums ", icon: "local_activity"))
-      ..sections.add(new PageSection("walking", "Walking Tours ", icon: "directions_walk")
+      ..sections.add(new PageSection("tourist-office", "Tourist Office", icon: "info_outline", icon_data: 0xe88f))
+      ..sections.add(new PageSection("parks", "Parks", icon: "nature_people", icon_data: 0xe407))
+      ..sections.add(new PageSection("neighborhoods", "Neighborhoods", icon: "zoom_out_map", icon_data: 0xe56b))
+      ..sections.add(new PageSection("museums", "Museums ", icon: "local_activity", icon_data: 0xe53f))
+      ..sections.add(new PageSection("walking", "Walking Tours ", icon: "directions_walk", icon_data: 0xe536)
         ..photoIdString = "walking-tours")
-      ..sections.add(new PageSection("seasonal-events", "Seasonal Events", icon: "event"))
-      ..sections.add(new PageSection("tallinn-card", "Tallinn Card", icon: "card_travel")),
+      ..sections.add(new PageSection("seasonal-events", "Seasonal Events", icon: "event", icon_data: 0xe878))
+      ..sections.add(new PageSection("tallinn-card", "Tallinn Card", icon: "card_travel", icon_data: 0xe8f8)),
     new ContentPage("Dining and Nightlife")
-      ..sections.add(new PageSection("cuisine", "Cuisine", icon: "local_dining"))
-      ..sections.add(new PageSection("dining", "Dining", icon: "restaurant"))
-      ..sections.add(new PageSection("nightlife", "Nightlife", icon: "local_bar")),
+      ..sections.add(new PageSection("cuisine", "Cuisine", icon: "local_dining", icon_data: 0xe556))
+      ..sections.add(new PageSection("dining", "Dining", icon: "restaurant", icon_data: 0xe56c))
+      ..sections.add(new PageSection("nightlife", "Nightlife", icon: "local_bar", icon_data: 0xe540)),
 //    new ContentPage("Accommodation"),
     new ContentPage("Elsewhere in Estonia")
-     ..sections.add(new PageSection("destinations-outside-tallinn", "Destinations", icon: "map"))
-      ..sections.add(new PageSection("getting-away", "Getting Away", icon: "explore")),
+     ..sections.add(new PageSection("destinations-outside-tallinn", "Destinations", icon: "map", icon_data: 0xe55b))
+      ..sections.add(new PageSection("getting-away", "Getting Away", icon: "explore", icon_data: 0xe87a)),
     new Page("Walking Tours", name: "walking-tours") 
       ..includeInNav = false,
     new Page("About", name: "about")..includeInNav = false,
@@ -45,7 +45,20 @@ class SiteStructure {
 
   SiteStructure();
 
-  ContentPage getPage(String id) => pages.firstWhere((Page i) => i.id == id);
+  static Page getPage(String id) => pages.firstWhere((Page i) => i.id == id);
+  static PageSection getSection(String id) {
+    PageSection section = null;
+    for (Page p in pages) {
+        if (p is ContentPage) {
+          section = p.sections.firstWhere((PageSection sec) => sec.id == id, orElse: () => null);
+          if (section != null) {
+            return section;
+          }
+        }
+    }
+    return section;
+
+  }
 
 
 }
